3d421cd62fd8a95eba38bcc4df4bc14c06fa4793
Oscar Cruz <cruzd.oscar@gmail.com>
2025-12-27 15:30:40 -0500

    Se añaden comentarios en el código SQL para mejorar la claridad de las consultas sobre viajes de taxis.

--- scripts_sql/01_trips_by_company.sql
+++ scripts_sql/01_trips_by_company.sql
@@ -1,14 +1,15 @@
--- Solicitud para obtener el número de viajes para cada empresa del 15 al 16 de noviembre
-SELECT 
+-- Solicitud SQL para obtener la cantidad de viajes para cada empresa de taxis los días 15 y 16 de noviembre de 2017
+
+SELECT
     c.company_name,
-    COUNT(t.trip_id) AS trips_amount
+    COUNT(t.trip_id) AS trips_amount -- Contamos la cantidad de viajes por empresa
 FROM 
     cabs AS c
 INNER JOIN 
-    trips AS t ON t.cab_id = c.cab_id
+    trips AS t ON t.cab_id = c.cab_id -- Unimos las tablas cabs y trips usando cab_id
 WHERE
-    t.start_ts::date IN ('2017-11-15', '2017-11-16')
+    t.start_ts::date IN ('2017-11-15', '2017-11-16') -- Filtramos por las fechas específicas
 GROUP BY 
-    c.company_name
+    c.company_name -- Agrupamos por el nombre de la empresa
 ORDER BY 
     trips_amount DESC;
--- scripts_sql/02_yellow_blue_cabs.sql
+++ scripts_sql/02_yellow_blue_cabs.sql
@@ -1,4 +1,5 @@
 -- Solicitud para obtener la cantidad de viajes para cada empresa de taxis cuyo nombre contenga las palabras 'Yellow' o 'Blue' del 1 al 7 de noviembre de 2017
+
 SELECT 
     c.company_name,
     COUNT(t.trip_id) AS trips_amount
@@ -7,6 +8,6 @@ FROM
 INNER JOIN 
     trips AS t ON t.cab_id = c.cab_id
 WHERE
-    (c.company_name LIKE '%Yellow%' OR c.company_name LIKE '%Blue%') AND (t.start_ts::date BETWEEN '2017-11-01' AND '2017-11-07')
+    (c.company_name LIKE '%Yellow%' OR c.company_name LIKE '%Blue%') AND (t.start_ts::date BETWEEN '2017-11-01' AND '2017-11-07') -- Filtramos por las empresas que contienen 'Yellow' o 'Blue' y por las fechas del 1 al 7 de noviembre de 2017
 GROUP BY 
     c.company_name;
